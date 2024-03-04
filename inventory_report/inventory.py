f01b3edd8c351781ab3c7d3093325cd41c2a47e7
Guilherme Muniz <guilherme.muniz91@gmail.com>
2024-03-04 16:18:39 -0300

    req5 feito

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,15 @@
+from inventory_report.product import Product
+from typing import Optional
+
+
 class Inventory:
-    pass
+
+    def __init__(self, data: Optional[list[Product]] = None):
+        self._data = [] if data is None else data
+
+    @property
+    def data(self) -> list[Product]:
+        return self._data
+
+    def add_data(self, data: list[Product]):
+        self._data.extend(data)
