f61d870f724ff1ee40a6b0f173f3598332393fe2
Guilherme Muniz <guilherme.muniz91@gmail.com>
2024-03-04 17:12:46 -0300

    req6 feito

--- inventory_report/reports/report.py
+++ inventory_report/reports/report.py
@@ -1,2 +1,26 @@
-class Report:
-    pass
+from inventory_report.inventory import Inventory
+from abc import abstractmethod
+from typing import Protocol
+
+
+class Report(Protocol):
+
+    @abstractmethod
+    def add_inventory(self, inventory: Inventory) -> None:
+        """
+        Adiciona um inventário ao relatório.
+
+        Args:
+            inventory: O inventário a ser adicionado.
+        """
+        pass
+
+    @abstractmethod
+    def generate(self) -> str:
+        """
+        Gera o relatório em formato string.
+
+        Returns:
+            String com o conteúdo do relatório.
+        """
+        pass
