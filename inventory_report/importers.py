f4f43661a75bf33a0dcd3a49bd1cb044308df718
Guilherme Muniz <guilherme.muniz91@gmail.com>
2024-03-04 15:55:11 -0300

    req 4 feito

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,6 +1,7 @@
 from typing import Dict, Type
 from abc import ABC, abstractmethod
 from inventory_report.product import Product
+import json
 
 
 class Importer(ABC):
@@ -22,8 +23,36 @@ class Importer(ABC):
         pass
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+
+    def import_data(self) -> list[Product]:
+        """
+        Importa os dados do arquivo JSON para uma lista de produtos.
+
+        Args:
+            path: Caminho do arquivo.
+
+        Returns:
+            Lista de produtos.
+        """
+
+        with open(self.path, "r") as file:
+            data = json.load(file)
+
+        products = []
+        for item in data:
+            product = Product(
+                id=item["id"],
+                product_name=item["product_name"],
+                company_name=item["company_name"],
+                manufacturing_date=item["manufacturing_date"],
+                expiration_date=item["expiration_date"],
+                serial_number=item["serial_number"],
+                storage_instructions=item["storage_instructions"],
+            )
+            products.append(product)
+
+        return products
 
 
 class CsvImporter:
